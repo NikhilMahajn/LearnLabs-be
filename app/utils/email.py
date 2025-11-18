f0889d5dd4d0c88c0657fed438b8bd5710359fb8
NikhilMahajn <mahajannikhil0016@gmail.com>
2025-11-18 19:14:06 +0530

    refactor email sending logic to improve error handling and validation

--- app/utils/email.py
+++ app/utils/email.py
@@ -3,19 +3,24 @@ from email.message import EmailMessage
 import smtplib
 
 def send_email(receiver_email,otp):
+    try:
 
-    if not receiver_email or not otp:
-        return {"status":"error","message" :"Missing required fields"}
+        if not receiver_email or not otp:
+            return {"status":"error","message" :"Missing required fields"}
 
-    smtp_host = os.getenv("SMTP_HOST", "smtp.gmail.com")  
-    smtp_port = int(os.getenv("SMTP_PORT", 587))
-    smtp_user = os.getenv("SMTP_USER")
-    smtp_pass = os.getenv("SMTP_PASS")
+        smtp_host = os.getenv("SMTP_HOST", "smtp.gmail.com")  
+        smtp_port = int(os.getenv("SMTP_PORT", 587))
+        smtp_user = os.getenv("SMTP_USER")
+        smtp_pass = os.getenv("SMTP_PASS")
 
-    if not smtp_user or not smtp_pass:
-        return {"status":"error","message" :"SMTP credentials are missing"}
+        if not smtp_user or not smtp_pass:
+            return {"status":"error","message" :"SMTP credentials are missing"}
 
-    SENDER_EMAIL = smtp_user
+        SENDER_EMAIL = smtp_user
+    except Exception as e:
+        print("Error:", str(e))
+        return {"status":"error","message" :str(e)}
+
     try:
         msg = EmailMessage()
         msg.set_content(f'Your OTP code is {otp}. It is valid for 10 minutes.')
